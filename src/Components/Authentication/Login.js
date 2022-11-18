1f3156fcae9c216e4a6903af28e528d3040f607a
Atharva Morankar <atharva.morankar09@gmail.com>
2022-11-18 23:56:54 +0530

    Check 2

--- src/Components/Authentication/Login.js
+++ src/Components/Authentication/Login.js
@@ -50,7 +50,7 @@ export default function Login() {
         </Card.Body>
       </Card>
       <div className='w-100 text-center mt-2'>
-        Don't have an account! <Link to="/signup">Sign Up</Link>
+        Don't have an account <Link to="/signup">Sign Up</Link>
       </div>
     </CenteredComponents>
   )
